58aa798d451f8ac318b7d22caabac8f014b86922
ar.Tzach <ar.tzachristas@gmail.com>
2025-07-26 12:41:19 +0300

    feat: footer info and product icon added

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './contact-us.component.html',
   styles: ``
 })
 export class ContactUsComponent {
 
+  protected readonly Date = Date;
 }
--- src/app/pages/olive-oil/olive-oil.component.ts
+++ src/app/pages/olive-oil/olive-oil.component.ts
@@ -1,46 +1,25 @@
 import {Component} from '@angular/core';
 import {ShopCardItem} from '../../shop-cards-item/shop-card-item';
 import {ShopCardsItemComponent} from '../../shop-cards-item/shop-cards-item.component';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-olive-oil',
-  imports: [ShopCardsItemComponent],
+  imports: [ShopCardsItemComponent, NgClass],
   templateUrl: './olive-oil.component.html',
   styles: '',
 })
 export class OliveOilComponent {
   shopItems: ShopCardItem[] = [{
     id: "item-001",
-    name: "Vintage Leather Bag",
-    year: 2022,
-    price: "$149.99",
+    name: "Extra Virgin Olive Oil",
+    year: 2025,
+    price: "$29.99",
     quantity: 12,
-    info: "Handcrafted leather bag made from premium materials.",
+    info: "Early harvest, extra virgin olive oil.",
     status: "available",
-    imageUrl: "img/olive-branch.webp",
-    imageSmUrl: "img/olive-branch.webp"
+    imageUrl: "img/product.webp",
+    imageSmUrl: "img/product.webp"
   },
-  {
-    id: "item-002",
-    name: "Vintage Leather Bag",
-    year: 2020,
-    price: "$149.99",
-    quantity: 12,
-    info: "Handcrafted leather bag made from premium materials.",
-    status: "available",
-    imageUrl: "img/olive-branch.webp",
-    imageSmUrl: "img/olive-branch.webp"
-  },
-  {
-    id: "item-003",
-    name: "Vintage Leather Bag",
-    year: 2024,
-    price: "$149.99",
-    quantity: 12,
-    info: "Handcrafted leather bag made from premium materials.",
-    status: "available",
-    imageUrl: "img/olive-branch.webp",
-    imageSmUrl: "img/olive-branch.webp"
-  }
   ];
 }
